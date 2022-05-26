ed4cdbf284ef43866b4a974d053ba33896d553e5
rishat <rishatrifa56@gmail.com>
2022-05-27 00:22:56 +0600

    load api done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgbvl.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgbvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
@@ -21,6 +21,23 @@ client.connect(err => {
     client.close();
 });
 
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('hammerMenufacturer').collection('tools');
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Rishat hammer manufacturer');
