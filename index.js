ead4e3831949ce5b9aa30f66c7312dc4d4c4d3e7
rishat <rishatrifa56@gmail.com>
2022-05-28 02:19:54 +0600

    booking post api done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -14,17 +15,18 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgbvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log("hammer connected");
-    // perform actions on the collection object
-    client.close();
-});
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log("hammer connected");
+//     // perform actions on the collection object
+//     client.close();
+// });
 
 async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('hammerMenufacturer').collection('tools');
+        const bookingCollection = client.db('hammerMenufacturer').collection('bookings');
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -32,7 +34,35 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.post('/login', async (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+            res.send({ token });
+        });
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await serviceCollection.findOne(query);
+            res.send(item);
+        });
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.get('/booking', async (req, res) => {
+            const query = {};
+            const cursor = bookingCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
     }
+
     finally {
 
     }
